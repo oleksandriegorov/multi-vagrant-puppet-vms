c300f30808e22152e38a0c1c212b9d3f6d957777
Oleksandr Iegorov <alex.yegorov@gmail.com>
2019-07-11 09:16:23 -0400

    Site.pp modifications

--- site.pp
+++ site.pp
@@ -2,7 +2,29 @@ node default {
 # Test message
   notify { "Debug output on ${hostname} node.": }
 
-  include ntp, git, lsys, lsys::pxe::tftp, lsys::pxe::dhcp
+  include ntp, git, lsys, lsys::pxe::tftp
+
+  lsys::dhcp::group { 'pxe_c100':
+    comment      => 'PXE c100',
+    pxe_settings => true,
+    host         => {
+      'rpmb'       => {
+        mac       => '00:50:56:8E:5B:30',
+        ip        => '216.55.156.40',
+        host_name => 'rpmb.carrierzone.com',
+      },
+      'bsys'       => {
+        mac       => '00:50:56:A5:E9:81',
+        ip        => '216.55.156.41',
+        host_name => 'bsys.carrierzone.com',
+      },
+      'web170c100' => {
+        mac       => '00:50:56:8E:23:B6',
+        ip        => '216.55.156.42',
+        host_name => 'web170c100.carrierzone.com',
+      }
+    }
+  }
 }
 
 node 'node01.example.com', 'node02.example.com' {
